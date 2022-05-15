16ce6f30a400ee414c64ca6de3f0b59511b543fa
mohammed slimani <mohammed.slimani@um5s.net.ma>
2022-05-15 16:21:47 +0200

    fix to not include leftDelimiter

--- assignments /interpolateSession/src/interpolate.js
+++ assignments /interpolateSession/src/interpolate.js
@@ -25,7 +25,7 @@ const interpolate = (value, session = {}, options = {}) => {
     options.rightDelimiter = makeSafeForRegex(options.rightDelimiter)
 
     // match a string that starts with the left delimiter, ends with right delimiter, but doesnt contain the rightDelimiter
-    const re = new RegExp(options.leftDelimiter + `([^${options.rightDelimiter}]+)` + options.rightDelimiter, 'g')
+    const re = new RegExp(options.leftDelimiter + `([^${options.leftDelimiter}]+)` + options.rightDelimiter, 'g')
 
     return value.replace(re, (...args) => {
         // we get the second catching group, since we start counting from 0 the second catching group is 1
